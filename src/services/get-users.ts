87d9d498921b1fcf76b2f3a4183e9c3adf8c7815
Firly Afriansyah <firlyafriansyah50@gmail.com>
2025-10-29 17:09:21 +0700

    feat(core): setup services for fetch user data

---
+++ src/services/get-users.ts
@@ -0,0 +1,10 @@
+import type { UserDataType } from "../models/users";
+
+export default async function GetUsersServices(): Promise<UserDataType[]> {
+  return new Promise((resolve, reject) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res: UserDataType[]) => resolve(res))
+      .catch((err) => reject(err.message));
+  });
+}
